124ed2265fd4b43ed612ff7fef0681cc9339e7b2
Miroslav Stoev <soboko@abv.bg>
2025-03-02 18:30:21 +0200

    update

---
+++ src/main/java/bg/home/books/web/HomeController.java
@@ -0,0 +1,25 @@
+package bg.home.books.web;
+
+import bg.home.books.model.entity.dto.BookDTO;
+import bg.home.books.service.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+    private final BookService bookService;
+
+    public HomeController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public String home(Model model) {
+        List<BookDTO> books = bookService.getAllBooks();
+        model.addAttribute("index", books);
+        return "index";
+    }
+}
